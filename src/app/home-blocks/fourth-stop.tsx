8b2aa27f951b102df8db0b20a4c18a3266762288
Aleksandr <alex.avramenko2004@gmail.com>
2024-04-12 06:11:20 -0400

    Second and fourth stops mobile done

--- src/app/home-blocks/fourth-stop.tsx
+++ src/app/home-blocks/fourth-stop.tsx
@@ -1,50 +1,53 @@
 import { fa4 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Image } from "@nextui-org/react";
+import { Divider, Image } from "@nextui-org/react";
 
 const FourthStop = () => {
     return (
-        <div className="w-full bg-[url('https://images.pexels.com/photos/265216/pexels-photo-265216.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')] bg-no-repeat bg-cover bg-center p-10 px-24" id="aboutus">
+        <div className="w-full bg-[url('https://images.pexels.com/photos/265216/pexels-photo-265216.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')] bg-no-repeat bg-cover bg-center p-5 px-3 lg:p-10 lg:px-24">
             
-            <div className="bg-gray-50 bg-opacity-60 pb-7">
-                <div className="px-16 py-7 pt-4 text-center">
+            <div className="bg-gray-50 bg-opacity-60 lg:pb-7">
+                <div className="px-5 py-3 lg:py-7 lg:px-16 lg:pt-4 text-center">
                     <FontAwesomeIcon size="3x" icon={fa4} className="border-2 border-black rounded-full p-5 px-6" />
-                    <h2 className="font-bold text-4xl my-3">Четверта зупинка - &quot;Збір врожаю&quot;</h2>
-                    <div className="grid grid-rows-4 gap-8  text-xl font-medium text-left leading-10">
-                        <div className="grid grid-cols-3 gap-8  justify-items-center items-center">
-                            <p className="col-span-2">Наша фінальна зупинка веде нас у світ дитячих спогадів та ностальгії, де кожен кадр має історію. &quot;Фотокартка як з дитинства&quot; — це ваша унікальна можливість зануритися в атмосферу минулого та створити незабутні спогади. Завершіть свою подорож по фермі, залишивши за собою слід у часі, фотографію, яка зможе розповісти про ваш незабутній день, проведений серед природи та історій, які нас оточують.</p>
+                    <h2 className="font-bold text-3xl lg:text-4xl lg:my-3">Четверта зупинка - &quot;Збір врожаю&quot;</h2>
+                    <div className="text-xl font-medium text-left leading-8 flex flex-col lg:grid lg:grid-cols-1 lg:grid-rows-4 lg:gap-8 lg:leading-10">
+                        <div className="flex flex-col gap-4 my-4 lg:grid lg:grid-rows-1 lg:grid-cols-3 lg:gap-8 lg:justify-items-center lg:items-center">
+                            <p className="lg:col-span-2">Наша фінальна зупинка веде нас у світ дитячих спогадів та ностальгії, де кожен кадр має історію. &quot;Фотокартка як з дитинства&quot; — це ваша унікальна можливість зануритися в атмосферу минулого та створити незабутні спогади. Завершіть свою подорож по фермі, залишивши за собою слід у часі, фотографію, яка зможе розповісти про ваш незабутній день, проведений серед природи та історій, які нас оточують.</p>
                             <Image
                                 alt="Harvesting"
                                 src="https://images.pexels.com/photos/9201367/pexels-photo-9201367.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
                                 width={400}
                             />
+                            <Divider className="lg:hidden" />
                         </div>
                         
-                        <div className="grid grid-cols-3 gap-8  justify-items-center items-center">
+                        <div className="flex flex-col-reverse gap-4 my-4 lg:grid lg:grid-rows-1 lg:grid-cols-3 lg:gap-8 lg:justify-items-center lg:items-center">
+                            <Divider className="lg:hidden" />
                             <Image
                                 alt="Harvest fruits and vegetables"
                                 src="https://images.pexels.com/photos/577697/pexels-photo-577697.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
                                 width={400}
                             />
-                            <p className="col-span-2">Ми створили особливі локації на території нашої ферми, де ви можете відтворити світлини з дитинства чи створити нові у стилі &quot;як колись&quot;. Чи то буде фотографія на тлі старовинного фермерського будинку, серед лугів з дикими квітами або поруч зі старим деревом, яке могло б бачити багато історій — кожне місце чекає, щоб стати частиною вашої історії.</p>
+                            <p className="lg:col-span-2">Ми створили особливі локації на території нашої ферми, де ви можете відтворити світлини з дитинства чи створити нові у стилі &quot;як колись&quot;. Чи то буде фотографія на тлі старовинного фермерського будинку, серед лугів з дикими квітами або поруч зі старим деревом, яке могло б бачити багато історій — кожне місце чекає, щоб стати частиною вашої історії.</p>
                         </div>
 
-                        <div className="grid grid-cols-3 gap-8  justify-items-center items-center">
-                            <p className="col-span-2">Ми запрошуємо вас принести старі сімейні фотографії, якщо ви хочете відтворити ці моменти, або просто прийти та дозволити нам допомогти створити нові спогади, які ви будете цінувати роками. Наш фотограф допоможе вам вибрати ідеальні ракурси та пози, щоб кожен знімок був особливим.</p>
+                        <div className="flex flex-col gap-4 my-4 lg:grid lg:grid-rows-1 lg:grid-cols-3 lg:gap-8 lg:justify-items-center lg:items-center">
+                            <p className="lg:col-span-2">Ми запрошуємо вас принести старі сімейні фотографії, якщо ви хочете відтворити ці моменти, або просто прийти та дозволити нам допомогти створити нові спогади, які ви будете цінувати роками. Наш фотограф допоможе вам вибрати ідеальні ракурси та пози, щоб кожен знімок був особливим.</p>
                             <Image
                                 alt="Harvesting"
                                 src="https://images.pexels.com/photos/3419719/pexels-photo-3419719.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
                                 width={400}
                             />
+                            <Divider className="lg:hidden" />
                         </div>
 
-                        <div className="grid grid-cols-3 gap-8  justify-items-center items-center">
+                        <div className="flex flex-col-reverse gap-4 my-4 lg:grid lg:grid-rows-1 lg:grid-cols-3 lg:gap-8 lg:justify-items-center lg:items-center">
                             <Image
                                 alt="Harvesting"
                                 src="https://images.pexels.com/photos/7782158/pexels-photo-7782158.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
                                 width={400}
                             />
-                            <p className="col-span-2">Ця зупинка — про зв&apos;язок поколінь, про теплі спогади та моменти, які об&apos;єднують сім&apos;ю. &quot;Фотокартка як з дитинства&quot; дає вам можливість не просто згадати минуле, а й створити нові історії, які одного дня стануть частиною вашого спадку.</p>
+                            <p className="lg:col-span-2">Ця зупинка — про зв&apos;язок поколінь, про теплі спогади та моменти, які об&apos;єднують сім&apos;ю. &quot;Фотокартка як з дитинства&quot; дає вам можливість не просто згадати минуле, а й створити нові історії, які одного дня стануть частиною вашого спадку.</p>
                         </div>
                         
                         
--- src/app/home-blocks/second-stop.tsx
+++ src/app/home-blocks/second-stop.tsx
@@ -1,50 +1,53 @@
 import { fa2 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Image } from "@nextui-org/react";
+import { Divider, Image } from "@nextui-org/react";
 
 const SecondStop = () => {
     return (
-        <div className="w-full bg-[url('https://images.pexels.com/photos/265216/pexels-photo-265216.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')] bg-no-repeat bg-cover bg-center p-10 px-24" id="aboutus">
+        <div className="w-full bg-[url('https://images.pexels.com/photos/265216/pexels-photo-265216.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')] bg-no-repeat bg-cover bg-center p-5 px-3 lg:p-10 lg:px-24">
             
-            <div className="bg-gray-50 bg-opacity-60 pb-7">
-                <div className="px-16 py-7 pt-4 text-center">
+            <div className="bg-gray-50 bg-opacity-60 lg:pb-7">
+                <div className="px-5 py-3 lg:py-7 lg:px-16 lg:pt-4 text-center">
                     <FontAwesomeIcon size="3x" icon={fa2} className="border-2 border-black rounded-full p-5 px-7" />
-                    <h2 className="font-bold text-4xl my-3">Друга зупинка - &quot;Збір врожаю&quot;</h2>
-                    <div className="grid grid-rows-4 gap-8 text-xl font-medium text-left leading-10">
-                        <div className="grid grid-cols-3 gap-8 justify-items-center items-center">
-                            <p className="col-span-2">Продовжуючи нашу подорож, ми переходимо до одного з найбільш очікуваних моментів у житті ферми — збору врожаю. Ця зупинка відкриває перед вами всю красу та важливість завершальної стадії аграрного циклу, де кожна рослина, кожен плід є результатом кропіткої праці та турботи</p>
+                    <h2 className="font-bold text-3xl lg:text-4xl lg:my-3">Друга зупинка - &quot;Збір врожаю&quot;</h2>
+                    <div className="text-xl font-medium text-left leading-8 flex flex-col lg:grid lg:grid-cols-1 lg:grid-rows-4 lg:gap-8 lg:leading-10">
+                        <div className="flex flex-col gap-4 my-4 lg:grid lg:grid-rows-1 lg:grid-cols-3 lg:gap-8 lg:justify-items-center lg:items-center">
+                            <p className="lg:col-span-2">Продовжуючи нашу подорож, ми переходимо до одного з найбільш очікуваних моментів у житті ферми — збору врожаю. Ця зупинка відкриває перед вами всю красу та важливість завершальної стадії аграрного циклу, де кожна рослина, кожен плід є результатом кропіткої праці та турботи</p>
                             <Image
                                 alt="Harvesting"
                                 src="https://images.pexels.com/photos/7658822/pexels-photo-7658822.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
                                 width={400}
                             />
+                            <Divider className="lg:hidden" />
                         </div>
                         
-                        <div className="grid grid-cols-3  gap-8  justify-items-center items-center">
+                        <div className="flex flex-col-reverse gap-4 my-4 lg:grid lg:grid-rows-1 lg:grid-cols-3 lg:gap-8 lg:justify-items-center lg:items-center">
+                            <Divider className="lg:hidden" />
                             <Image
                                 alt="Harvest fruits and vegetables"
                                 src="https://images.pexels.com/photos/7728874/pexels-photo-7728874.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
                                 width={400}
                             />
-                            <p className="col-span-2">На цій зупинці ви не просто спостерігачі, а активні учасники. Вам буде запропоновано взяти участь у зборі врожаю разом з нашою командою. Ви відчуєте на собі, як це — збирати яблука у фруктовому саду, знімати помідори та огірки з грядок, або ж збирати соковиті ягоди прямо з кущів. Це унікальна можливість доторкнутися до самої суті фермерства, відчути зв&apos;язок з землею та зрозуміти, скільки зусиль потрібно для того, щоб виростити якісні та екологічно чисті продукти</p>
+                            <p className="lg:col-span-2">На цій зупинці ви не просто спостерігачі, а активні учасники. Вам буде запропоновано взяти участь у зборі врожаю разом з нашою командою. Ви відчуєте на собі, як це — збирати яблука у фруктовому саду, знімати помідори та огірки з грядок, або ж збирати соковиті ягоди прямо з кущів. Це унікальна можливість доторкнутися до самої суті фермерства, відчути зв&apos;язок з землею та зрозуміти, скільки зусиль потрібно для того, щоб виростити якісні та екологічно чисті продукти</p>
                         </div>
 
-                        <div className="grid grid-cols-3 gap-8  justify-items-center items-center">
-                            <p className="col-span-2">Під час збору врожаю ви також дізнаєтесь більше про різні сорти рослин, методи їх вирощування, а також про техніки зберігання та переробки продукції. Ми розкажемо вам, як правильно визначати стиглість плодів, щоб збір врожаю був максимально ефективним та безвідходним</p>
+                        <div className="flex flex-col gap-4 my-4 lg:grid lg:grid-rows-1 lg:grid-cols-3 lg:gap-8 lg:justify-items-center lg:items-center">
+                            <p className="lg:col-span-2">Під час збору врожаю ви також дізнаєтесь більше про різні сорти рослин, методи їх вирощування, а також про техніки зберігання та переробки продукції. Ми розкажемо вам, як правильно визначати стиглість плодів, щоб збір врожаю був максимально ефективним та безвідходним</p>
                             <Image
                                 alt="Harvesting"
                                 src="https://images.pexels.com/photos/18551846/pexels-photo-18551846/free-photo-of-jars-with-vegetables.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
                                 width={400}
                             />
+                            <Divider className="lg:hidden" />
                         </div>
 
-                        <div className="grid grid-cols-3 gap-8  justify-items-center items-center">
+                        <div className="flex flex-col-reverse gap-4 my-4 lg:grid lg:grid-rows-1 lg:grid-cols-3 lg:gap-8 lg:justify-items-center lg:items-center">
                             <Image
                                 alt="Harvesting"
                                 src="https://images.pexels.com/photos/3184192/pexels-photo-3184192.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
                                 width={400}
                             />
-                            <p className="col-span-2">Збір врожаю — це не просто важлива частина аграрного процесу, а й час святкування та подяки землі за її щедрість. Це можливість відчути себе частиною великої природної системи, яка живить і підтримує життя на нашій планеті. Після трудових буднів на вас чекає смачне застілля з продуктами, вирощеними власноруч, де кожен зможе оцінити справжній смак і якість свіжозібраного врожаю</p>
+                            <p className="lg:col-span-2">Збір врожаю — це не просто важлива частина аграрного процесу, а й час святкування та подяки землі за її щедрість. Це можливість відчути себе частиною великої природної системи, яка живить і підтримує життя на нашій планеті. Після трудових буднів на вас чекає смачне застілля з продуктами, вирощеними власноруч, де кожен зможе оцінити справжній смак і якість свіжозібраного врожаю</p>
                         </div>
                         
                         
