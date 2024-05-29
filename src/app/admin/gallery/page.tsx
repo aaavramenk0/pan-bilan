3cec3f7f22f14a516b4b408c682c6cec5d81b885
Aleksandr <alex.avramenko2004@gmail.com>
2024-05-29 04:56:22 -0400

    Gallery Admin Update

--- src/app/admin/gallery/page.tsx
+++ src/app/admin/gallery/page.tsx
@@ -1,9 +1,22 @@
 import Gallery from "@/app/gallery/page";
+import { uploadImage } from "@/lib/cloudinary";
+
 
 export default function AdminGallery() {
+  
   return (
     <div>
-        <Gallery />
+      <div className="max-w-[90vw] m-auto bg-slate-300 bg-opacity-55 p-5">
+        <h2 className="font-bold text-2xl"> Завантажити нове фото в галерею </h2>
+        <form action={uploadImage}>
+          <input type="file" name="image" id="image"  className="my-4"/>
+          <br />
+          <input type="submit" value="Завантажити фото" className=" border-1 border-black p-2"/>
+        </form>
+      </div>
+      
+      
+      <Gallery />
     </div>
   );
 }
